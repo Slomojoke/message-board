0ad71d6624af995770d22954046e269ad7d3cbc5
Mendax <asalgado7875@gmail.com>
2022-09-04 16:13:37 -0400

    First commit

---
+++ mini-message-board/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "My name is Amando and I go to school",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
